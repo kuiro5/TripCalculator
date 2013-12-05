f8c14a37a0e42496327d8a6599fbe49927e75175
Joshua Kuiros <joshkuiros@gmail.com>
2013-12-05 16:21:27 -0500

    draws direction routes from point->point

--- TripCalculator/TripCalculator/jjkMapViewController.m
+++ TripCalculator/TripCalculator/jjkMapViewController.m
@@ -13,7 +13,8 @@
 @property (strong, nonatomic) CLGeocoder *geocoder;
 @property CLLocationCoordinate2D startingLocation;
 @property CLLocationCoordinate2D destinationLocation;
-//@property CLLocationCoordinate2D coordinate;
+@property (strong, nonatomic) NSMutableArray *geocodedAddresses;
+@property (strong, nonatomic) NSMutableArray *routePoints;
 
 @end
 
@@ -23,50 +24,148 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
+        NSLog(@"check");
         self.mapView.showsUserLocation = YES;
-        //self.coordinate = CLLocationCoordinate2DMake(0.0, 0.0);
+        
     }
     return self;
 }
 
+-(void)putObject:(MKMapItem*)mapItem
+{
+   
+    NSLog(@"inside put object");
+    [self.geocodedAddresses addObject:mapItem];
+}
+
 -(void)geocodeAddress:(NSMutableArray*)locations
 {
+    
+    
+    __block NSInteger i = 0;
+    
+    
     for(NSString *address in locations)
     {
     
     self.geocoder = [[CLGeocoder alloc] init];
-    
     __block CLLocationCoordinate2D coordinate;
+    __block MKMapItem *srcItem;
+   // __block NSMutableArray *geocodedAddresses =[[NSMutableArray alloc]initWithCapacity:4];
     
     [self.geocoder geocodeAddressString:address completionHandler:^(NSArray *placemarks, NSError *error) {
+        
         if ([placemarks count] > 0)
         {
             NSLog(@"inside placemark count");
             CLPlacemark *placemark = [placemarks objectAtIndex:0];
             CLLocation *location = placemark.location;
             coordinate = location.coordinate;
-            NSLog(@"latitude %f", coordinate.latitude);
-            NSLog(@"longitude %f", coordinate.longitude);
+            
+            [self.routePoints addObject:location];
+            
+            MKPlacemark *srcMark = [[MKPlacemark alloc] initWithCoordinate:coordinate addressDictionary:nil];
+            srcItem = [[MKMapItem alloc] initWithPlacemark:srcMark];
+            [self putObject:srcItem];
+            //[self.geocodedAddresses addObject:srcItem];
+            i++;
+            
             [self updateMapView:coordinate withName:address];
-//            self.coordinatesLabel.text = [NSString stringWithFormat:@"%f, %f", coordinate.latitude, coordinate.longitude];
-            if ([placemark.areasOfInterest count] > 0)
-            {
-                //NSString *areaOfInterest = [placemark.areasOfInterest objectAtIndex:0];
-                //self.nameLabel.text = areaOfInterest;
-                NSLog(@"found");
-            }
-            else
-            {
-                NSLog(@"error");
-            }
         }
+        
+        
+        NSLog(@"%d",i);
+        
+        if(i == 2)
+        {
+            [self scaleMapView:self.routePoints];
+            [self calculateDirections:self.geocodedAddresses];
+        }
+        
     }];
     
-    
+        
     }
+    
     
 }
 
+-(void)scaleMapView:(NSMutableArray*)routePoints
+{
+    CLLocationDegrees maxLat = -90;
+    CLLocationDegrees maxLon = -180;
+    CLLocationDegrees minLat = 90;
+    CLLocationDegrees minLon = 180;
+    
+    for(int i = 0; i < routePoints.count; i++)
+    {
+        CLLocation *currentLocation = [self.routePoints objectAtIndex:i];
+        if(currentLocation.coordinate.latitude > maxLat)
+            maxLat = currentLocation.coordinate.latitude;
+        if(currentLocation.coordinate.latitude < minLat)
+            minLat = currentLocation.coordinate.latitude;
+        if(currentLocation.coordinate.longitude > maxLon)
+            maxLon = currentLocation.coordinate.longitude;
+        if(currentLocation.coordinate.longitude < minLon)
+            minLon = currentLocation.coordinate.longitude;
+    }
+    
+    MKCoordinateRegion region;
+    region.center.latitude = (maxLat + minLat) / 2;
+    region.center.longitude = (maxLon + minLon) / 2;
+    region.span.latitudeDelta = (maxLat - minLat) * 1.2;
+    region.span.longitudeDelta = (maxLon - minLon) * 1.2;
+    
+    [self.mapView setRegion:region];
+    
+    
+}
+
+-(void)calculateDirections:(NSMutableArray*)addresses
+{
+    NSLog(@"getting called");
+    
+    MKMapItem *temporary =[addresses objectAtIndex:0];
+    
+    MKDirectionsRequest *request = [[MKDirectionsRequest alloc] init];
+    
+    request.source = temporary;
+    
+    temporary =[addresses objectAtIndex:1];
+    
+    request.destination = temporary;
+    
+    request.requestsAlternateRoutes = YES;
+    MKDirections *directions =
+    [[MKDirections alloc] initWithRequest:request];
+    
+    [directions calculateDirectionsWithCompletionHandler:
+     ^(MKDirectionsResponse *response, NSError *error) {
+         if (error) {
+             // Handle Error
+         } else {
+             [self showRoute:response];
+         }
+     }];
+    
+}
+
+-(void)showRoute:(MKDirectionsResponse *)response
+{
+    for (MKRoute *route in response.routes)
+    {
+        [self.mapView addOverlay:route.polyline level:MKOverlayLevelAboveRoads];
+    }
+}
+
+- (MKOverlayRenderer *)mapView:(MKMapView *)mapView rendererForOverlay:(id < MKOverlay >)overlay
+{
+    MKPolylineRenderer *renderer =
+    [[MKPolylineRenderer alloc] initWithOverlay:overlay];
+    renderer.strokeColor = [UIColor blueColor];
+    renderer.lineWidth = 5.0;
+    return renderer;
+}
 
 
 
@@ -75,6 +174,10 @@
     
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    
+    self.mapView.delegate = self;
+    self.geocodedAddresses = [[NSMutableArray alloc] init];
+    self.routePoints = [[NSMutableArray alloc] init];
     
     NSMutableArray *locations = [[NSMutableArray alloc]initWithObjects:self.starting, self.destination, nil];
     
@@ -86,7 +189,7 @@
 {
     
     
-    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(currentCoordinate, 800, 800);
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(currentCoordinate, 20000, 20000);
     [self.mapView setRegion:region];
     
     //[self.mapView removeAnnotations:[self.mapView annotations]];
